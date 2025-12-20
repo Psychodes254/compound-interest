9e9d89579848a2f079e37d99a55fc6e3ce2b681d
Psychodes254 <justusmasika80@gmail.com>
2025-12-20 16:11:27 +0300

    Refined the naming of variables

--- save_for_a_house.py
+++ save_for_a_house.py
@@ -1,16 +1,20 @@
 def saving_for_a_house():
-    y_sal = float(input("Enter your yearly salary: "))
-    saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+    # User inputs
+    annual_sal = float(input("Enter your yearly salary: "))
+    sal_percent_save = float(input("Enter the percent of your salary to save, as a decimal: "))
     home_cost = float(input("Enter the cost of your dream home: "))
-    dwn_portion = 0.25
+    
+    # Down payment portion 25% of home cost
+    dwn_payment_portion = 0.25
+    
+    # Annual earn return rate
     return_rate = 0.05
     
     # Target savings (down payment)
-    target_saving = home_cost * dwn_portion
+    target_saving = home_cost * dwn_payment_portion
     
-    # Monthly savings contribution
     # Annual savings:
-    yearly = y_sal * saved
+    yearly = annual_sal * sal_percent_save
     
     # Monthly contribution:
     monthly = yearly / 12
@@ -19,13 +23,14 @@ def saving_for_a_house():
     rate = return_rate / 12
     
     # Initiate previous month contribution
-    mon_check = 0
+    monthly_check = 0
     
     # initiate months count
     months = 0
     
-    while mon_check < target_saving:
-        mon_check += (mon_check * rate) + monthly
+    # Iterate over if the monthly contributions is still lower than target savings while adding months
+    while monthly_check < target_saving:
+        monthly_check += (monthly_check * rate) + monthly
         months += 1
     
     # Return no. of months
